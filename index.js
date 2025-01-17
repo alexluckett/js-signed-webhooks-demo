c86628b121b7e0ba4bbb401af305e3d02234f4d2
Alex Luckett <alexgluckett@gmail.com>
2025-01-17 14:30:44 +0000

    .

--- index.js
+++ index.js
@@ -33,16 +33,22 @@ console.log("Here's what the request will look like", request);
 /**
  * FORM CONSUMER (my-backend-api) - VALID SIGNATURE
  */
-const isValid = verifySignature(publicKey, signature, request.body);
-console.assert(isValid == true) // it matches as expected
+const isValid = verifySignature(publicKey, signature, requestPayload);
+console.assert(isValid === true); // it matches as expected
 
 /**
- * FORM CONSUMER (my-backend-api) - INVALID SIGNATURE
+ * FORM CONSUMER (my-backend-api) - INVALID SIGNATURE - FORGED KEY
+ */
+const { publicKey: anotherPublicKey } = generateKey();
+
+const isValid2 = verifySignature(anotherPublicKey, signature, requestPayload);
+console.assert(isValid2 === false); // we're verifying with the wrong public key, the signature won't match
+
+/**
+ * FORM CONSUMER (my-backend-api) - INVALID SIGNATURE - MANIPULATED PAYLOAD
  */
 requestPayload.context.state.applicantsName = "my malicious beneficiary";
 requestPayload.context.state.doYouWantFreeMoney = "yes";
 
-const isValid2 = verifySignature(publicKey, signature, requestPayload);
-console.assert(isValid2 == false) // we've modified the payload, the signature shouldn't match
-
-console.log("All worked!");
+const isValid3 = verifySignature(publicKey, signature, requestPayload);
+console.assert(isValid3 === false); // we've modified the payload, the signature won't match
